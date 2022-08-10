d0f3e81a9541dd02e1365a863f5ef505116afeea
EC2 Default User <ec2-user@ip-172-31-26-113.ap-northeast-1.compute.internal>
2022-08-10 14:13:32 +0000

    [Update]4-22

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,12 +1,21 @@
 class PostImage < ApplicationRecord
   has_one_attached :image
   belongs_to :user
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   
-  def get_image
-    unless image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io:File.open(file_path),filename: 'default-image.jpg', content_type: 'image/hpeg')
+  validates :shop_name, presence: true
+  validates :image, presence: true
+
+  def get_profile_image
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      profile_image.attach(io:File.open(file_path),filename: 'default-image.jpg', content_type: 'image/hpeg')
     end
-    image
+    profile_image.variant(resize_to_limit: [100, 100]).processed
+  end
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
   end
 end
